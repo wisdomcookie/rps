a2b3b447c7211f1768f05db9cbb25aae3670df1b
Nathan_Dinh <dinhtrongnhan2015@gmail.com>
2023-04-13 21:50:32 -0400

    Add buttons for player selection and displays score and results

--- script.js
+++ script.js
@@ -40,45 +40,101 @@ function playRound(playerChoice, computerChoice){
             break;
     }
 
+    
+    let restring = "";
     switch(result){
         case "LOSS":
-            return `You Lose! ${computerChoice} beats ${playerChoice}`;
+            player2++;
+            restring = `You Lose! ${computerChoice} beats ${playerChoice}`;
+            break;
         case "DRAW":
-            return `Draw! You both picked ${playerChoice}`;
+            restring = `Draw! You both picked ${playerChoice}`;
+            break;
         case "VICTORY":
-            return `You Win! ${playerChoice} beats ${computerChoice}`;
+            player1++;
+            restring = `You Win! ${playerChoice} beats ${computerChoice}`;
+            break;
     }
+    updateScore();
+    gameCheck();
+    return restring;
 }
 
-function game(){
-    let count = 0;
-    let wins = 0;
-    let losses = 0;
+// function game(){
+//     let count = 0;
+//     let wins = 0;
+//     let losses = 0;
 
-    while(count < 5){
-        playerSelection = prompt("Enter choice:");
-        computerSelection = getComputerChoice();
-        result = playRound(playerSelection, computerSelection);
+//     while(count < 5){
+//         playerSelection = prompt("Enter choice:");
+//         computerSelection = getComputerChoice();
+//         result = playRound(playerSelection, computerSelection);
         
-        if(result.includes("Lose")){
-            losses++;
-        }
-        else if(result.includes("Win")){
-            wins++;
-        }
-        count++;
-    }
+//         if(result.includes("Lose")){
+//             losses++;
+//         }
+//         else if(result.includes("Win")){
+//             wins++;
+//         }
+//         count++;
+//     }
 
-    if(wins > losses){
-        return "You win";
-    }
-    else if(losses > wins){
-        return "You lose";
-    }
-    else{
-        return "Draw";
-    }
+//     if(wins > losses){
+//         return "You win";
+//     }
+//     else if(losses > wins){
+//         return "You lose";
+//     }
+//     else{
+//         return "Draw";
+//     }
 
+// }
+
+let player1 = 0;
+let player2 = 0;
+
+function gameCheck(){
+    if(player1 === 5 || player2 === 5){
+        game.textContent = "Player 1 wins!";
+    }
+    else if(player2 === 5){
+        game.textContent = "Player 2 wins!";
+    }
 }
 
-console.log(game());
+function updateScore(){
+    score.textContent = `Player 1: ${player1}, Player 2: ${player2}`;
+}
+
+const round = document.querySelector('.round');
+const score = document.querySelector('.score');
+const game = document.querySelector('.game');
+
+const rockbtn = document.querySelector('.rock');
+rockbtn.addEventListener('click', () => {
+    let playerChoice = "Rock";
+    result = playRound(playerChoice, getComputerChoice());
+    console.log(result);
+    round.textContent = result;
+});
+
+const paperbtn = document.querySelector('.paper');
+paperbtn.addEventListener('click', () => {
+    let playerChoice = "Paper";
+    result = playRound(playerChoice, getComputerChoice());
+    console.log(result);
+    round.textContent = result;
+});
+
+const scissorsbtn = document.querySelector('.scissors');
+scissorsbtn.addEventListener('click', () => {
+    let playerChoice = "Scissors";
+    result = playRound(playerChoice, getComputerChoice());
+    console.log(result);
+    round.textContent = result;
+});
+
+
+
+//console.log(game());
